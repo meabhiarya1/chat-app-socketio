9ec291f8925bf9ba6dee579ec502a7e56947fe88
Abhishek kumar <abhi.arya97161@gmail.com>
2025-03-03 14:54:00 +0530

    Initialize backend and frontend structure with Vite and React setup

---
+++ Backend/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+app.use(cors({ origin: "*" }));
+
+const server = http.createServer(app);
+
+const users = {};
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+console.log(users);
+io.on("connection", (socket) => {
+  console.log("User connected");
+  socket.on("join", (username) => {
+    users[username] = socket.id;
+    console.log(users);
+    io.emit("userlist", Object.keys(users));
+  });
+
+  socket.on("privatemessage", ({ sender, recipent, message }) => {
+    const recipentSocketId = users[recipent];
+    console.log("Recipient Socket ID:", recipentSocketId);
+
+    if (recipentSocketId) {
+      io.emit("recieveprivatemessage", { sender, message, recipent });
+      console.log(
+        `✅ Message sent to ${recipent} (${recipentSocketId}): ${message}`
+      );
+    } else {
+      console.log(`Recipient ${recipent} not found.`);
+    }
+  });
+  // Group Broadcasting
+  socket.on("groupmessage", ({ sender, message }) => {
+    io.emit("recievemessage", { sender, message }); // Send to all users
+  });
+
+  socket.on("disconnect", () => {
+    for (let username in users) {
+      if (users[username] === socket.id) {
+        delete users[username];
+        break;
+      }
+    }
+
+    io.emit("userlist", Object.keys(users));
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,122 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+function App() {
+  const [userName, setUserName] = useState("");
+  const [users, setUsers] = useState([]);
+  const [recipent, setRecipent] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const userRef = useRef(null);
+  const socket = io("http://localhost:4000");
+  useEffect(() => {
+    socket.on("userlist", (users) => {
+      console.log(users);
+      setUsers(users);
+    });
+
+    socket.on("recieveprivatemessage", ({ sender, message, recipent }) => {
+      // console.log(`📩 Received message from ${sender}: ${message}`);
+
+      if (recipent === userName) {
+        setMessages((messages) => [...messages, { sender, message }]);
+      }
+    });
+    socket.on("recievemessage", ({ sender, message }) => {
+      console.log(`📩 Received message from ${sender}: ${message}`);
+
+      setMessages((messages) => [...messages, { sender, message }]);
+    });
+    return () => {
+      socket.off("userlist");
+      socket.off("recievemessage");
+      socket.off("recieveprivatemessage");
+    };
+  }, [userName]);
+
+  const handleJoin = () => {
+    const userName = userRef.current.value;
+    if (userName) {
+      setUserName(userName);
+      socket.emit("join", userName);
+    }
+  };
+  const sendPrivateMessage = () => {
+    console.log(recipent, message);
+    if (recipent && message) {
+      socket.emit("privatemessage", { sender: userName, recipent, message });
+      setMessages((prev) => [...prev, { sender: "You", message }]);
+      setMessage("");
+    }
+  };
+  const sendGroupMessage = () => {
+    if (message) {
+      socket.emit("groupmessage", { sender: userName, message });
+      setMessages((prev) => [...prev, { sender: "You(Group)", message }]);
+      setMessage("");
+    }
+  };
+  return (
+    <>
+      <div>
+        {!userName ? (
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              // value={userName}
+              ref={userRef}
+              // onChange={(e) => {
+              //   setUserName(e.target.value);
+              // }}
+            />
+            <button onClick={handleJoin}>Join</button>
+          </div>
+        ) : (
+          <div>
+            <h3>Welcome, {userName}</h3>
+            <h4>Online Users : </h4>
+            <ul>
+              {users
+                .filter((user) => user !== userName)
+                .map((user) => (
+                  <li key={user}>{user}</li>
+                ))}
+            </ul>
+            <h4>Send Message</h4>
+            <select onChange={(e) => setRecipent(e.target.value)}>
+              <option value="">Select recipent</option>
+              {users
+                .filter((users) => users !== userName)
+                .map((user) => (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                ))}
+            </select>
+            <input
+              type="text"
+              placeholder="Enter message"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+            <button onClick={sendPrivateMessage}>Send Message</button>
+            <button onClick={sendGroupMessage}>send to group</button>
+            <h4>Chats</h4>
+            <div>
+              {messages.map(({ sender, message }, index) => (
+                <p key={index}>
+                  {sender} : {message}
+                </p>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
